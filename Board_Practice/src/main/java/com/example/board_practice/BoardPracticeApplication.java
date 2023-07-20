37175b80c06af41b0a1b956948238c32a8153ac3
Yoohaeun <haspring@dsm.hs.kr>
2023-07-20 22:48:02 +0900

    ⚙️ :: 프로젝트설정

--- Board_Practice/src/main/java/com/example/board_practice/BoardPracticeApplication.java
+++ Board_Practice/src/main/java/com/example/board_practice/BoardPracticeApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootstudy;
+package com.example.board_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootStudyApplication {
+public class BoardPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootStudyApplication.class, args);
+		SpringApplication.run(BoardPracticeApplication.class, args);
 	}
 
 }
--- Board_Practice/src/test/java/com/example/board_practice/BoardPracticeApplicationTests.java
+++ Board_Practice/src/test/java/com/example/board_practice/BoardPracticeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springbootstudy;
+package com.example.board_practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootStudyApplicationTests {
+class BoardPracticeApplicationTests {
 
 	@Test
 	void contextLoads() {
