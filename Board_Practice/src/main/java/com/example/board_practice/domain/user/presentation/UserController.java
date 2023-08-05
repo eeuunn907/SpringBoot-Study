46787eabafaa9f77fd79a55544b2eb0c02e737b7
Yoohaeun <haspring@dsm.hs.kr>
2023-08-05 22:26:05 +0900

    📑 :: 패키지 이름 수정

--- Board_Practice/src/main/java/com/example/board_practice/domain/user/presentation/UserController.java
+++ Board_Practice/src/main/java/com/example/board_practice/domain/user/presentation/UserController.java
@@ -3,21 +3,22 @@ package com.example.board_practice.domain.user.presentation;
 
 import com.example.board_practice.domain.user.presentation.dto.request.SignUpRequest;
 import com.example.board_practice.domain.user.service.UserSignUpService;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @RestController
-public class UserSignUpController {
+public class UserController {
     private final UserSignUpService userSignUpService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
-    public void signup(@RequestBody @Valid SignUpRequest request){
-        userSignUpService.SignUp(request);
+    public void signup(@RequestBody @Valid SignUpRequest request) {
+        userSignUpService.execute(request);
     }
 
 }
