02eaede60f446b499d0a7620a7e235a12500b623
Yoohaeun <haspring@dsm.hs.kr>
2023-08-07 17:20:30 +0900

    ♻️ :: 코드 정리

--- Board_Practice/src/main/java/com/example/board_practice/domain/user/domain/User.java
+++ Board_Practice/src/main/java/com/example/board_practice/domain/user/domain/User.java
@@ -1,7 +1,11 @@
 package com.example.board_practice.domain.user.domain;
 
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
