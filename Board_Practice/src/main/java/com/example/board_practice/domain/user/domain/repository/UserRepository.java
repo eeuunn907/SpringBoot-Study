1e3986ec43c9985e7e7797f6168bc60034bef082
Yoohaeun <haspring@dsm.hs.kr>
2023-08-07 17:20:13 +0900

    ♻️ :: 코드 정리

--- Board_Practice/src/main/java/com/example/board_practice/domain/user/domain/repository/UserRepository.java
+++ Board_Practice/src/main/java/com/example/board_practice/domain/user/domain/repository/UserRepository.java
@@ -1,11 +1,10 @@
 package com.example.board_practice.domain.user.domain.repository;
 
 import com.example.board_practice.domain.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByAccountId(String accountId);
 }
