a0362be3ae0d0552bcbda85558748733b9816523
Yoohaeun <haspring@dsm.hs.kr>
2023-08-07 17:19:58 +0900

    ♻️ :: 코드 정리

--- Board_Practice/src/main/java/com/example/board_practice/global/security/SecurityConfig.java
+++ Board_Practice/src/main/java/com/example/board_practice/global/security/SecurityConfig.java
@@ -6,41 +6,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
-                .formLogin().disable()
-                .cors()
+            .csrf().disable()
+            .formLogin().disable()
+            .cors()
 
-                .and()
+            .and()
 
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-                .and()
-                .authorizeHttpRequests()
+            .and()
+            .authorizeHttpRequests()
 
+            .antMatchers("/user/signup").permitAll()
+            .antMatchers("/feed").permitAll()
 
-                .antMatchers("/user/signup").permitAll()
-                .antMatchers("/feed").permitAll()
-
-                .and().build();
+            .and().build();
     }
 
     @Bean
-    public PasswordEncoder encoder(){
+    public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 }
