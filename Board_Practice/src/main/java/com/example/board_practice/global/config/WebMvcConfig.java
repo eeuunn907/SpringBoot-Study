9c0074ee80225eac511304a807476fe39a231c3b
Yoohaeun <haspring@dsm.hs.kr>
2023-08-07 17:21:14 +0900

    ♻️ :: 코드 정리

--- Board_Practice/src/main/java/com/example/board_practice/global/config/WebMvcConfig.java
+++ Board_Practice/src/main/java/com/example/board_practice/global/config/WebMvcConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*");
     }
 }
 
