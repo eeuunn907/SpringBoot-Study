e8488613d49629f5c3039849dddc325bedf578a1
Yoohaeun <haspring@dsm.hs.kr>
2023-08-07 17:20:58 +0900

    ♻️ :: 코드 정리

--- Board_Practice/src/main/java/com/example/board_practice/domain/user/presentation/UserController.java
+++ Board_Practice/src/main/java/com/example/board_practice/domain/user/presentation/UserController.java
@@ -3,16 +3,20 @@ package com.example.board_practice.domain.user.presentation;
 
 import com.example.board_practice.domain.user.presentation.dto.request.SignUpRequest;
 import com.example.board_practice.domain.user.service.UserSignUpService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @RestController
 public class UserController {
+
     private final UserSignUpService userSignUpService;
 
     @ResponseStatus(HttpStatus.CREATED)
