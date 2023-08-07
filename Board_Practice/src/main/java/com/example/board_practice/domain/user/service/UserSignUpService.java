3ef70c2642b3c9baa459cf58bb520496a8262810
Yoohaeun <haspring@dsm.hs.kr>
2023-08-07 17:21:06 +0900

    ♻️ :: 코드 정리

--- Board_Practice/src/main/java/com/example/board_practice/domain/user/service/UserSignUpService.java
+++ Board_Practice/src/main/java/com/example/board_practice/domain/user/service/UserSignUpService.java
@@ -3,25 +3,25 @@ package com.example.board_practice.domain.user.service;
 import com.example.board_practice.domain.user.domain.User;
 import com.example.board_practice.domain.user.domain.repository.UserRepository;
 import com.example.board_practice.domain.user.presentation.dto.request.SignUpRequest;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserSignUpService {
+
     private final UserRepository userRepository;
 
     @Transactional
     public void execute(SignUpRequest request) {
 
         User user = User.builder()
-                .accountId(request.getAccountId())
-                .name(request.getName())
-                .password(request.getPassword())
-                .phoneNumber(request.getPhoneNumber())
-                .build();
+            .accountId(request.getAccountId())
+            .name(request.getName())
+            .password(request.getPassword())
+            .phoneNumber(request.getPhoneNumber())
+            .build();
 
         userRepository.save(user);
     }
