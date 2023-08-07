d2171ef12e3bee323c937c9f45d1ab1478197d0d
Yoohaeun <haspring@dsm.hs.kr>
2023-08-07 17:20:45 +0900

    ♻️ :: 코드 정리

--- Board_Practice/src/main/java/com/example/board_practice/domain/user/exception/UserNotFoundException.java
+++ Board_Practice/src/main/java/com/example/board_practice/domain/user/exception/UserNotFoundException.java
@@ -7,7 +7,7 @@ public class UserNotFoundException extends CustomException {
 
     public static final CustomException EXCEPTION = new UserNotFoundException();
 
-    private UserNotFoundException(){
+    private UserNotFoundException() {
         super(ErrorCode.USER_NOT_FOUND);
     }
 }
