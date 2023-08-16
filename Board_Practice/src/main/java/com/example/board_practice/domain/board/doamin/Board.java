dae1692ca375bab9b10fa7700bab8d2eecebce07
Yoohaeun <haspring@dsm.hs.kr>
2023-08-17 07:55:04 +0900

    ⚡️ :: Board Entity 생성

---
+++ Board_Practice/src/main/java/com/example/board_practice/domain/board/doamin/Board.java
@@ -0,0 +1,58 @@
+package com.example.board_practice.domain.board.doamin;
+
+
+import com.example.board_practice.domain.user.domain.User;
+import com.example.board_practice.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Board extends BaseTimeEntity {
+
+    @NotNull
+    @Size(max = 20)
+    private String title;
+
+    @NotNull
+    @Size(max = 500)
+    private String content;
+
+    @NotNull
+    private Integer likeCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @Builder
+    public Board(String title,String content,String likeCount,User user){
+        this.title = title;
+        this.content = content;
+        this.likeCount = likeCount;
+        this.user = user;
+    }
+
+    public void modifyBoard(String title,String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public void addLikeCount(){
+        this.likeCount += 1;
+    }
+
+    public void minusLikeCount(){
+        this.likeCount -= 1;
+    }
+}
